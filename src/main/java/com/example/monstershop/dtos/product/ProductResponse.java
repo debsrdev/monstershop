9aedd43236fcfba11c3326b02ad0cc09ad31b0d9
Débora Rubio <debora.rubio.soliva@gmail.com>
2025-06-17 18:15:03 +0200

    add CORS config to connect backend to frontend, add reviews on Product

--- src/main/java/com/example/monstershop/dtos/product/ProductResponse.java
+++ src/main/java/com/example/monstershop/dtos/product/ProductResponse.java
@@ -1,12 +1,18 @@
 package com.example.monstershop.dtos.product;
 
+import com.example.monstershop.dtos.review.ReviewResponse;
+import com.example.monstershop.models.Review;
+
+import java.util.List;
+
 public record ProductResponse(
         String name,
         double price,
         String imageUrl,
         double rating,
         int reviewCount,
-        boolean featured
+        boolean featured,
+        List<ReviewResponse> reviews
 ) {
 
 }
